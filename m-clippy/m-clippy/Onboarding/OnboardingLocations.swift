f1092d6cc53e4203b9309824f517249f78edab9d
Lorenz Hänggi <lolo8304@gmail.com>
2020-09-19 20:06:13 +0200

    locations

---
+++ m-clippy/m-clippy/Onboarding/OnboardingLocations.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingLocations.swift
+//  m-clippy
+//
+//  Created by Lorenz Hänggi on 19.09.20.
+//
+
+import SwiftUI
+
+struct OnboardingLocations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingLocations_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingLocations()
+    }
+}
